331ae66844402a5e141a22927144a7fab5ab75f2
Akshat Pathak <94177449+akshatpathak26@users.noreply.github.com>
2023-01-17 13:58:45 +0530

    Update AccessControl.sol

--- AccessControl.sol
+++ AccessControl.sol
@@ -40,33 +40,4 @@ contract AccessControl {
 
 
 }
-    bytes32 private constant ADMIN = keccak256(abi.encodePacked("ADMIN"));
 
-    //0x2db9fd3d099848027c2383d0a083396f6c41510d7acfd92adc99b6cffcf31e96
-    bytes32 private constant USER = keccak256(abi.encodePacked("USER"));
-    
-    modifier onlyRole(bytes32 _role){
-        require(roles[_role][msg.sender], "not authorized");
-        _;
-    }
-
-    constructor() {
-        _grantRole(ADMIN, msg.sender);
-    }
-
-    function _grantRole(bytes32 _role, address _account) internal {
-        roles[_role][_account] = true;
-        emit GrantRole(_role , _account);
-    }
-
-    function grantRole(bytes32 _role, address _account) external onlyRole(ADMIN){
-        _grantRole(_role, _account);
-    }
-
-    function revokeRole(bytes32 _role, address _account) external onlyRole(ADMIN){
-        roles[_role][_account] = false;
-        emit RevokeRole(_role , _account);
-    }
-
-
-}
